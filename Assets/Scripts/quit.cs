8650f8fc9f835b077b43c91a16fddf7f88a348c7
Gisu-PC\Gisu <mirva.tapola@gmail.com>
2018-04-28 23:31:09 +0300

    Eka

---
+++ Assets/Scripts/quit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quit : MonoBehaviour {
+
+    public Canvas start;
+    public Button exit;
+
+	public void doQuit(){
+        Debug.Log("Has quit");
+        Application.Quit();
+    }
+}
